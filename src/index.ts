6b20a46b4f05bf0b7216d35289cb42d6d3d28e36
edris <edris.emperator@gmail.com>
2023-02-18 00:07:53 +0330

    add CSVFileReader and read football.csv

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./matchResult";
+
+const reader = new CsvFileReader("football.csv");
+reader.read();
+const dateOfFirstMatch = reader.data[0][0]
+
+console.log(dateOfFirstMatch);
+
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
